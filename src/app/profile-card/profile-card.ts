97c5453771f91bd89db3aaf4d1c36d02b6681bd9
kemi-p <kpole@dvtsoftware.com>
2026-04-10 14:23:09 +0200

    name html file for the card properly, set up the ts file

---
+++ src/app/profile-card/profile-card.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from "@angular/common";
+import { Component, Input } from "@angular/core";
+
+
+@Component({
+    selector: 'profile-card',
+    imports: [ CommonModule],
+    templateUrl: './profile-card.html',
+    styleUrl:'./profile-card.scss'
+})
+
+export class ProfileCard{
+   
+
+}
