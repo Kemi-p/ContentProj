2b50ff755236c4ae8ba17700c332f407dbeb8dd5
kemi-p <kpole@dvtsoftware.com>
2026-04-10 14:15:01 +0200

    displa notif banners with diff colors

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NotificationBanner } from './notif-banner/notif-banner';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NotificationBanner],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- src/app/notif-banner/notif-banner.ts
+++ src/app/notif-banner/notif-banner.ts
@@ -1,11 +1,22 @@
-import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Component, Input } from "@angular/core";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
     selector: 'notification-banner',
-    template: ``
+    imports: [MatIconModule, CommonModule, MatButtonModule],
+    templateUrl: './notif-banner.html',
+    styleUrl:'./notif-banner.scss'
 })
 
 export class NotificationBanner{
-    
+    @Input() type: 'success'|'error'|'warning' = 'success'
+
+    dismissed = false
+
+    dismiss(){
+        this.dismissed = true
+    }
 
 }
